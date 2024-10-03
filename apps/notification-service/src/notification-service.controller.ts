b7c33f88fb9a0146fcae3bf56d908356836515d3
Jeffin <jeffinj787@gmail.com>
2024-10-03 21:04:31 +0100

    Initial commit

---
+++ apps/notification-service/src/notification-service.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { NotificationServiceService } from './notification-service.service';
+
+@Controller()
+export class NotificationServiceController {
+  constructor(private readonly notificationServiceService: NotificationServiceService) {}
+
+  @Get()
+  getHello(): string {
+    return this.notificationServiceService.getHello();
+  }
+}
